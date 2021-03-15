1b22a58f1e22bb819ee7a813b57530538c2a4581
Oisin <G00347603@gmit.ie>
2021-03-15 14:46:39 +0000

    Setting up Variables for Salarys

--- src/main/java/ie/gmit/SalaryTax.java
+++ src/main/java/ie/gmit/SalaryTax.java
@@ -1,4 +1,14 @@
 package ie.gmit;
 
 public class SalaryTax {
-}
+    private double hourlyRate;
+    private double weeklyHours;
+    private double salary;
+    private double incomeTax;
+    private double prsi;
+    private double usc;
+
+    public SalaryTax() {
+
+    }
+}   
