e50dbc370c96bd730a140550198b5756080609b0
Latif <latifyahia98@gmail.com>
2021-03-15 20:12:08 +0000

    added some error checking for businesstax class and also trying to test one of the methods i wrote in the business class but currently the test is not working as im having problems with maven

--- src/main/java/ie/gmit/BusinessTax.java
+++ src/main/java/ie/gmit/BusinessTax.java
@@ -1,7 +1,44 @@
 package ie.gmit;
 
 public class BusinessTax {
-    int operatingExpense;
-    int revenue;
-    int grossProfit = revenue - operatingExpense;
+
+
+    double operatingExpense;
+    double revenue;
+
+
+    public BusinessTax(double operatingExpense, double revenue)
+    {
+        setRevenue(revenue);
+        setOperatingExpense(operatingExpense);
+    }
+
+    public double getOperatingExpense()
+    {
+        return operatingExpense;
+    }
+
+    public void setOperatingExpense(double operatingExpense)
+    {
+        if(operatingExpense >= 0)
+        {
+            this.operatingExpense = operatingExpense;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Operating Expense cannot be negative");
+        }
+
+    }
+
+    public double getRevenue()
+    {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue)
+    {
+        this.revenue = revenue;
+    }
+
 }
