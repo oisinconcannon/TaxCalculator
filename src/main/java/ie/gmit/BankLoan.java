790dd204306af80d17754624076c6d1330fa1ec8
Brian Sharkey <briansharkey07@gmail.com>
2021-03-16 10:05:06 +0000

    Added Bank loan class with variables and constructor

---
+++ src/main/java/ie/gmit/BankLoan.java
@@ -0,0 +1,24 @@
+/*
+* Brian Sharkey - g00301661@gmit.ie
+* Program to calculate Bank loans.
+* Software and Electronic Engineering 2021
+*
+* */
+
+package ie.gmit;
+
+
+public class BankLoan{
+
+    public int yearsOnLoan;
+    public double purchasePrice;
+    public double loanAmount;
+    public double interestRate = 0.029/12;
+
+    public BankLoan() {
+
+    }
+
+
+
+}
