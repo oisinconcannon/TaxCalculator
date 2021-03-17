2c4ba44a2690f3127c6650902171e65a363852e7
Oisin <G00347603@gmit.ie>
2021-03-17 11:29:37 +0000

    writing test code, code for income tax written

--- src/main/java/ie/gmit/SalaryTax.java
+++ src/main/java/ie/gmit/SalaryTax.java
@@ -1,3 +1,9 @@
+/*
+Oisin Concannon
+G00347603@gmit.ie
+This feature will calculate personal tax for an individual
+ Software and Electronic Engineering 2021
+ */
 package ie.gmit;
 
 public class SalaryTax {
@@ -5,10 +11,50 @@ public class SalaryTax {
     private double weeklyHours;
     private double salary;
     private double incomeTax;
+    private double upperIncomeTax;
     private double prsi;
     private double usc;
 
-    public SalaryTax() {
+    public SalaryTax(double salary) {
+        setSalary(salary);
+
 
     }
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(double salary)
+    {
+        if(salary >= 17000)
+        {
+            this.salary = salary;
+        }
+        else
+        {
+            throw new IllegalArgumentException("You have entered an ineligible value for salary(minimum 17,000)");
+        }
+
+    }
+
+    public double IncomeTax()
+    {
+        salary = getSalary();
+        if(salary >=17000) {
+            if (salary > 35300) {
+                upperIncomeTax = (salary - 35300) * .40;
+                incomeTax = (salary * .2) - 3300 + upperIncomeTax;
+            } else {
+                incomeTax = (salary * .2) + 3300;
+            }
+            return incomeTax;
+        }
+        else{
+            throw new IllegalArgumentException("You have entered an ineligible value for salary(minimum 17,000),therefore cannot calculate income Tax");
+        }
+    }
+
+
+
 }   
--- src/test/java/ie/gmit/SalaryTaxTest.java
+++ src/test/java/ie/gmit/SalaryTaxTest.java
@@ -1,4 +1,28 @@
+/*
+Oisin Concannon
+G00347603@gmit.ie
+This feature will calculate personal tax for an individual
+ Software and Electronic Engineering 2021
+ */
 package ie.gmit;
 
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SalaryTaxTest {
+
+
+    @Test
+    void incomeTaxTest(){
+        SalaryTax s = new SalaryTax(30000);
+        assertEquals(2700,s.IncomeTax());
+    }
+    @Test
+    void incomeTaxTestFail(){
+        SalaryTax s = new SalaryTax(15000);
+        assertThrows(IllegalArgumentException.class,() -> s.IncomeTax());
+    }
+
 }
