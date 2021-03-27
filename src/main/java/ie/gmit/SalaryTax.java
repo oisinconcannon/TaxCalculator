0ebcb21daf328880dc19fe67dd285fe2c0c27692
Oisin <G00347603@gmit.ie>
2021-03-27 20:22:27 +0000

    finishing my version 1 code

--- src/main/java/ie/gmit/SalaryTax.java
+++ src/main/java/ie/gmit/SalaryTax.java
@@ -1,14 +1,110 @@
 package ie.gmit;
 
 public class SalaryTax {
-    private double hourlyRate;
-    private double weeklyHours;
-    private double salary;
-    private double incomeTax;
-    private double prsi;
-    private double usc;
 
-    public SalaryTax() {
+    private String name;
+    private String company;
+    private int employmentNumber;
+    private double salary;
+
+
+
+    public SalaryTax(String name, String company, int employmentNumber,double salary ) {
+        setName(name);
+        setCompany(company);
+        setEmploymentNumber(employmentNumber);
+        setSalary(salary);
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        if(name.length() > 2)
+        {
+            if(name.length() < 25)
+            {
+                this.name = name;
+            }
+            else {
+                throw new IllegalArgumentException("Name too long");
+            }
+        }
+        else
+        {
+            throw new IllegalArgumentException("Name too short");
+        }
 
     }
+    public String getCompany()
+    {
+        return company;
+    }
+
+    public void setCompany(String company)
+    {
+        if(company.length() > 2)
+        {
+            if(company.length() < 15)
+            {
+                this.company = company;
+            }
+            else {
+                throw new IllegalArgumentException("Company name too long");
+            }
+        }
+        else
+        {
+            throw new IllegalArgumentException("Company name too short");
+        }
+
+    }
+    public double getEmploymentNumber()
+    {
+        return employmentNumber;
+    }
+
+    public void setEmploymentNumber(int employmentNumber)
+    {
+        if(String.valueOf(employmentNumber).length() >2)
+        {
+            if(String.valueOf(employmentNumber).length() <11)
+            {
+                this.employmentNumber = employmentNumber;
+            }
+            else
+            {
+                throw new IllegalArgumentException("Employment ID too long");
+            }
+        }
+        else
+        {
+            throw new IllegalArgumentException("Employment ID too short");
+        }
+
+    }
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(double salary)
+    {
+        if(salary >= 17000)
+        {
+            this.salary = salary;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Invalid Salary");
+        }
+
+    }
+
+
+
+
+
 }   
--- src/test/java/ie/gmit/SalaryTaxTest.java
+++ src/test/java/ie/gmit/SalaryTaxTest.java
@@ -1,4 +1,32 @@
 package ie.gmit;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SalaryTaxTest {
+    private SalaryTax salaryTest;
+    @BeforeEach
+    void setup()
+    {
+        salaryTest = new SalaryTax("oisin","gmit",1234,30000);
+    }
+
+    @Test
+    void salaryTest()
+    {
+       SalaryTax s = new SalaryTax("oisin","gmit",1234,30000);
+       assertEquals(30000,s.getSalary());
+    }
+    @Test
+    void salaryFail()
+    {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> new SalaryTax("oisin","gmit",1234,10000));
+        assertEquals("Invalid Salary", e.getMessage());
+
+    }
+
+
 }
