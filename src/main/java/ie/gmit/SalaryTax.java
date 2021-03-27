e2eed2bb6e4b6021f5c311913e3505707ef53163
Oisin <G00347603@gmit.ie>
2021-03-27 20:33:09 +0000

    merging

--- src/main/java/ie/gmit/SalaryTax.java
+++ src/main/java/ie/gmit/SalaryTax.java
@@ -12,152 +12,79 @@ public class SalaryTax {
     private String company;
     private int employmentNumber;
     private double salary;
-<<<<<<< HEAD
 
 
-
-    public SalaryTax(String name, String company, int employmentNumber,double salary ) {
+    public SalaryTax(String name, String company, int employmentNumber, double salary) {
         setName(name);
         setCompany(company);
         setEmploymentNumber(employmentNumber);
         setSalary(salary);
     }
-    public String getName()
-    {
+
+    public String getName() {
         return name;
     }
 
-    public void setName(String name)
-    {
-        if(name.length() > 2)
-        {
-            if(name.length() < 25)
-            {
+    public void setName(String name) {
+        if (name.length() > 2) {
+            if (name.length() < 25) {
                 this.name = name;
-            }
-            else {
+            } else {
                 throw new IllegalArgumentException("Name too long");
             }
-        }
-        else
-        {
+        } else {
             throw new IllegalArgumentException("Name too short");
         }
 
     }
-    public String getCompany()
-    {
+
+    public String getCompany() {
         return company;
     }
 
-    public void setCompany(String company)
-    {
-        if(company.length() > 2)
-        {
-            if(company.length() < 15)
-            {
+    public void setCompany(String company) {
+        if (company.length() > 2) {
+            if (company.length() < 15) {
                 this.company = company;
-            }
-            else {
+            } else {
                 throw new IllegalArgumentException("Company name too long");
             }
-        }
-        else
-        {
+        } else {
             throw new IllegalArgumentException("Company name too short");
         }
 
     }
-    public double getEmploymentNumber()
-    {
+
+    public double getEmploymentNumber() {
         return employmentNumber;
     }
 
-    public void setEmploymentNumber(int employmentNumber)
-    {
-        if(String.valueOf(employmentNumber).length() >2)
-        {
-            if(String.valueOf(employmentNumber).length() <11)
-            {
+    public void setEmploymentNumber(int employmentNumber) {
+        if (String.valueOf(employmentNumber).length() > 2) {
+            if (String.valueOf(employmentNumber).length() < 11) {
                 this.employmentNumber = employmentNumber;
-            }
-            else
-            {
+            } else {
                 throw new IllegalArgumentException("Employment ID too long");
             }
-        }
-        else
-        {
+        } else {
             throw new IllegalArgumentException("Employment ID too short");
         }
 
     }
-    public double getSalary()
-    {
+
+    public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary)
-    {
-        if(salary >= 17000)
-        {
+    public void setSalary(double salary) {
+        if (salary >= 17000) {
             this.salary = salary;
-        }
-        else
-        {
+        } else {
             throw new IllegalArgumentException("Invalid Salary");
         }
 
     }
-
-
-=======
-    private double incomeTax;
-    private double upperIncomeTax;
-    private double prsi;
-    private double usc;
-
-    public SalaryTax(double salary) {
-        setSalary(salary);
-
-
-    }
-    public double getSalary()
-    {
-        return salary;
-    }
-
-    public void setSalary(double salary)
-    {
-        if(salary >= 17000.00)
-        {
-            this.salary = salary;
-        }
-        else
-        {
-            throw new IllegalArgumentException("You have entered an ineligible value for salary(minimum 17,000)");
-        }
-
-    }
-
-  /*  public double IncomeTax()
-    {
-        salary = getSalary();
-        if(salary >=17000) {
-            if (salary > 35300) {
-                upperIncomeTax = (salary - 35300) * .40;
-                incomeTax = (salary * .2) - 3300 + upperIncomeTax;
-            } else {
-                incomeTax = (salary * .2) - 3300;
-            }
-            return incomeTax;
-        }
-        else{
-            throw new IllegalArgumentException("You have entered an ineligible value for salary(minimum 17,000),therefore cannot calculate income Tax");
-        }
-    }*/
->>>>>>> master
+}
 
 
 
-}   
