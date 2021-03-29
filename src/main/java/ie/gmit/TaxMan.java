aea3f945467ceba445690bff4c0c1ee31f4c350a
Latif <latifyahia98@gmail.com>
2021-03-29 15:48:40 +0100

    build test demo

--- src/main/java/ie/gmit/TaxMan.java
+++ src/main/java/ie/gmit/TaxMan.java
@@ -4,6 +4,7 @@ public class TaxMan {
 
     public static void main(String[] args)
     {
-        System.out.println("Welcome to your tax calculator");
+        System.out.println("Welcome to your tax calculator\n\n");
+        System.out.println("What tax would you like to calculate");
     }
 }
